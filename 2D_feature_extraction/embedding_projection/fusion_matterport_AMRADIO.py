8323a83fe26c87f15cfbf6874ea4c93e152cb3bb
TyroneLi <1940686092lijinlong@gmail.com>
2025-04-02 11:16:27 +0200

    init commit

---
+++ 2D_feature_extraction/embedding_projection/fusion_matterport_AMRADIO.py
@@ -0,0 +1,252 @@
+import os
+import torch
+import imageio
+import argparse
+from os.path import join, exists
+import numpy as np
+from glob import glob
+from tqdm import tqdm, trange
+# add the parent directory to the path
+import sys
+sys.path.append('..')
+
+from torch.nn import functional as F
+from torchvision.transforms.functional import pil_to_tensor
+
+import torchvision.transforms as transforms
+from PIL import Image
+from fusion_util import (extract_lseg_img_feature, 
+                         PointCloudToImageMapper, 
+                         save_fused_feature_with_locs_noAligned, 
+                         get_matterport_camera_data)
+
+
+def get_args():
+    # command line args
+    parser = argparse.ArgumentParser(
+        description='Multi-view feature fusion of LSeg on Matterport3D.')
+    parser.add_argument('--data_dir', type=str, help='Where is the base logging directory')
+    parser.add_argument('--output_dir', type=str, help='Where is the base logging directory')
+    parser.add_argument('--scan_dir', type=str, default="/home/jinlong.li/datasets/ScanNetV2/scannetv2_download/scans", help='')
+    parser.add_argument('--voxel_size', type=float, default=0.02, help='')
+    parser.add_argument('--split', type=str, default='test', help='split: "train"| "val" | "test" ')
+    parser.add_argument('--process_id_range', nargs='+', default=None, help='the id range to process')
+    parser.add_argument('--prefix', type=str, default='AMRADIO', help='prefix for the output file')
+    parser.add_argument('--save_aligned', type=bool, default=True, help='whether to save aligned features')
+
+    # Hyper parameters
+    parser.add_argument('--hparams', default=[], nargs="+")
+    args = parser.parse_args()
+    return args
+
+def process_one_scene(data_path, out_dir, args):
+    '''Process one scene.'''
+
+    img_dim = (640, 512)
+    
+    # short hand
+    num_rand_file_per_scene = args.num_rand_file_per_scene
+    
+    feat_dim = args.feat_dim
+    assert feat_dim == 1024
+    depth_scale = args.depth_scale
+    point2img_mapper = args.point2img_mapper
+    keep_features_in_memory = args.keep_features_in_memory
+    evaluator = args.evaluator
+
+    # load 3D data (point cloud, color and the corresponding labels)
+    locs_in = torch.load(data_path)[0]
+    n_points = locs_in.shape[0]
+
+    # obtain all camera views related information (specificially for Matterport)
+    intrinsics, poses, img_dirs, scene_id, num_img = \
+            get_matterport_camera_data(data_path, locs_in, args)
+    if num_img == 0:
+        print('no views inside {}'.format(scene_id))
+        return 1
+
+    n_interval = num_rand_file_per_scene    
+    n_finished = 0
+    for n in range(n_interval):
+
+        if exists(join(out_dir, scene_id +'_%d.pt'%(n))):
+            n_finished += 1
+            print(scene_id +'_%d.pt'%(n) + ' already done!')
+            continue
+    if n_finished == n_interval:
+        return 1
+
+    device = torch.device('cpu')
+    # # extract image features and keep them in the memory
+    # # default: False (extract image on the fly)
+    # if keep_features_in_memory and evaluator is not None:
+    #     img_features = []
+    #     for img_dir in tqdm(img_dirs):
+    #         img_features.append(extract_lseg_img_feature(img_dir, transform, evaluator))
+
+    n_points_cur = n_points
+    counter = torch.zeros((n_points_cur, 1), device=device)
+    sum_features = torch.zeros((n_points_cur, feat_dim), device=device)
+
+    ################ Feature Fusion ###################
+    vis_id = torch.zeros((n_points_cur, num_img), dtype=int, device=device)
+    for img_id, img_dir in enumerate(tqdm(img_dirs)):
+        # load pose
+        pose = poses[img_id]
+
+        # load per-image intrinsic
+        intr = intrinsics[img_id]
+
+        # load depth
+        depth_dir = img_dir.replace('color', 'depth')
+        _, img_type, yaw_id = img_dir.split('/')[-1].split('_')
+        depth_dir = depth_dir[:-8] + 'd'+img_type[1] + '_' + yaw_id[0] + '.png'
+        depth = imageio.v3.imread(depth_dir) / depth_scale  # convert to meter
+
+        # calculate the 3d-2d mapping based on the depth
+        mapping = np.ones([n_points, 4], dtype=int)
+        mapping[:, 1:4] = point2img_mapper.compute_mapping(pose, locs_in, depth, intr)
+        if mapping[:, 3].sum() == 0: # no points corresponds to this image, skip
+            continue
+
+        mapping = torch.from_numpy(mapping).to(device)
+        mask = mapping[:, 3]
+        vis_id[:, img_id] = mask
+        
+        # if keep_features_in_memory:
+        #     feat_2d = img_features[img_id].to(device)
+        # else:
+        #     feat_2d = extract_lseg_img_feature(img_dir, transform, evaluator).to(device)
+
+        # feat_2d_3d = feat_2d[:, mapping[:, 1], mapping[:, 2]].permute(1, 0)
+        
+        image = Image.open(img_dir).convert('RGB')
+        image = pil_to_tensor(image).to(dtype=torch.float32, device='cuda')
+        image.div_(255.0)
+        image = image.unsqueeze(dim=0)
+        
+        with torch.no_grad():
+            nearest_res = evaluator.get_nearest_supported_resolution(*image.shape[-2:])
+            image = F.interpolate(image, nearest_res, mode='bilinear', align_corners=False) 
+            summary, spatial_features = evaluator(image, feature_fmt='NCHW')
+            assert spatial_features.ndim == 4
+            feat_2d = spatial_features
+            
+        # import pdb;pdb.set_trace()
+        
+        feat_2d = torch.nn.functional.interpolate(
+            feat_2d, 
+            size=(512, 640), 
+            mode='bicubic', 
+            align_corners=False).squeeze(0) # 240, 320, 1024
+        
+        feat_2d_3d = feat_2d[:, mapping[:, 1], mapping[:, 2]].permute(1, 0).to(device)
+
+        counter[mask!=0]+= 1
+        sum_features[mask!=0] += feat_2d_3d[mask!=0]
+
+    counter[counter==0] = 1e-5
+    feat_bank = sum_features/counter
+    point_ids = torch.unique(vis_id.nonzero(as_tuple=False)[:, 0])
+
+    # if args.save_aligned:
+    #     # print("hhhhh")
+    #     save_fused_feature_with_locs(feat_bank, point_ids, locs_in, n_points, out_dir, scene_id, args)
+    # else:
+    #     save_fused_feature(feat_bank, point_ids, n_points, out_dir, scene_id, args)
+        
+    # save_fused_feature(feat_bank, point_ids, n_points, out_dir, scene_id, args)
+    
+    save_fused_feature_with_locs_noAligned(feat_bank, point_ids, locs_in, n_points, out_dir, scene_id, args)
+    
+    # sameAs_save_fused_feature_matterport(feat_bank, point_ids, n_points, out_dir, scene_id, args)
+
+def main(args):
+    seed = 1457
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+    #### Dataset specific parameters #####
+    img_dim = (640, 512)
+    # img_dim = (320, 240)
+    depth_scale = 4000.0
+    args.depth_scale = depth_scale
+    
+    transforms_mean = [0.48145466, 0.4578275, 0.40821073]
+    transforms_std = [0.26862954, 0.26130258, 0.27577711]
+    
+    #######################################
+    visibility_threshold = 0.02
+
+    split = args.split
+    data_dir = args.data_dir
+    out_dir = args.output_dir
+    args.feat_dim = 1024 # Dinov2 feature dimension
+    os.makedirs(out_dir, exist_ok=True)
+    data_root = join(data_dir, 'matterport_3d')
+    data_root_2d = join(data_dir,'matterport_2d')
+    args.data_root_2d = data_root_2d
+
+    args.cut_num_pixel_boundary = 10 # do not use the features on the image boundary
+    args.keep_features_in_memory = False # keep image features in the memory, very expensive
+    process_id_range = args.process_id_range
+
+    # if split== 'train': # for training set, export a chunk of point cloud
+    #     args.n_split_points = 20000
+    #     args.num_rand_file_per_scene = 5
+    # else: # for the validation set, export the entire point cloud instead of chunks
+    #     args.n_split_points = 2000000
+    #     args.num_rand_file_per_scene = 1
+    
+    if split== 'train': # for training set, export a chunk of point cloud
+        # args.n_split_points = 20000
+        # args.num_rand_file_per_scene = 5
+        
+        args.n_split_points = 2000000
+        args.num_rand_file_per_scene = 1
+        
+    else: # for the validation set, export the entire point cloud instead of chunks
+        args.n_split_points = 2000000
+        args.num_rand_file_per_scene = 1
+
+
+    # model_version="radio_v2.5-h" # for RADIOv2.5-H model (ViT-H/16)
+    model_version="radio_v2.5-l" # for RADIOv2.5-L model (ViT-L/16)
+    #model_version="radio_v2.5-b" # for RADIOv2.5-B model (ViT-B/16)
+    #model_version="e-radio_v2" # for E-RADIO
+    model = torch.hub.load('NVlabs/RADIO', 'radio_model', version=model_version, progress=True, skip_validation=True)
+    model.cuda().eval()
+
+    args.evaluator = model
+
+    # calculate image pixel-3D points correspondances
+    args.point2img_mapper = PointCloudToImageMapper(
+            image_dim=img_dim,
+            visibility_threshold=visibility_threshold,
+            cut_bound=args.cut_num_pixel_boundary)
+
+    data_paths = sorted(glob(join(data_root, split, '*.pth')))
+    total_num = len(data_paths)
+
+    id_range = None
+    if process_id_range is not None:
+        id_range = [int(process_id_range[0].split(',')[0]), int(process_id_range[0].split(',')[1])]
+
+    print("total_num : ", total_num)
+    
+    for i in trange(total_num):
+        if id_range is not None and \
+           (i<id_range[0] or i>id_range[1]):
+            print('skip ', i, data_paths[i])
+            continue
+
+        process_one_scene(data_paths[i], out_dir, args)
+
+if __name__ == "__main__":
+    args = get_args()
+    print("Arguments:")
+    print(args)
+
+    main(args)
+    
---
+++ 2D_feature_extraction/embedding_projection/fusion_scannet_AMRADIO.py
@@ -0,0 +1,222 @@
+import os
+import torch
+import imageio
+import argparse
+from os.path import join, exists
+import numpy as np
+from glob import glob
+from tqdm import tqdm, trange
+# add the parent directory to the path
+import sys
+sys.path.append('..')
+
+from torch.nn import functional as F
+from torchvision.transforms.functional import pil_to_tensor
+from PIL import Image
+
+from models import LSeg_MultiEvalModule
+from modules.lseg_module import LSegModule
+from encoding.models.sseg import BaseNet
+import torchvision.transforms as transforms
+
+from fusion_util import (extract_lseg_img_feature, 
+                         PointCloudToImageMapper, 
+                         save_fused_feature_with_locs, 
+                         adjust_intrinsic, 
+                         make_intrinsic, 
+                         save_fused_feature_with_locs_noAligned,
+                         save_fused_feature,
+                         sameAs_save_fused_feature_scannet)
+
+
+def get_args():
+    # command line args
+    parser = argparse.ArgumentParser(
+        description='Multi-view feature fusion of LSeg on ScanNet.')
+    parser.add_argument('--data_dir', type=str, help='Where is the base logging directory')
+    parser.add_argument('--output_dir', type=str, help='Where is the base logging directory')
+    parser.add_argument('--scan_dir', type=str, default="/home/jinlong.li/datasets/ScanNetV2/scannetv2_download/scans", help='')
+    parser.add_argument('--voxel_size', type=float, default=0.02, help='')
+    parser.add_argument('--lseg_model', 
+                        type=str, 
+                        default='./lexicon3d/demo_e200.ckpt', help='Where is the LSeg checkpoint')
+    parser.add_argument('--split', type=str, default='val', help='split: "train"| "val"')
+    parser.add_argument('--process_id_range', nargs='+', default=None, help='the id range to process')
+    parser.add_argument('--prefix', type=str, default='AMRADIO', help='prefix for the output file')
+    parser.add_argument('--save_aligned', type=bool, default=True, help='whether to save aligned features')
+    
+
+    # Hyper parameters
+    parser.add_argument('--hparams', default=[], nargs="+")
+    args = parser.parse_args()
+    return args
+
+
+def process_one_scene(data_path, out_dir, args):
+
+    # short hand
+    scene_id = data_path.split('/')[-1].split('_vh')[0]
+
+    feat_dim = args.feat_dim
+    point2img_mapper = args.point2img_mapper
+    depth_scale = args.depth_scale
+    keep_features_in_memory = args.keep_features_in_memory
+    evaluator = args.evaluator
+    # transform = args.transform
+
+
+    # load 3D data (point cloud)
+    locs_in = torch.load(data_path)[0]
+    n_points = locs_in.shape[0]
+
+    if exists(join(out_dir, args.prefix+'_features', scene_id + '.pt')):
+        print(scene_id +'.pt' + ' already exists, skip!')
+        return 1
+
+    # short hand for processing 2D features
+    scene = join(args.data_root_2d, scene_id)
+    img_dirs = sorted(glob(join(scene, 'color/*')), key=lambda x: int(os.path.basename(x)[:-4]))
+    num_img = len(img_dirs)
+    device = torch.device('cpu')
+
+    # extract image features and keep them in the memory
+    # default: False (extract image on the fly)
+    if keep_features_in_memory and evaluator is not None:
+        img_features = []
+        for img_dir in tqdm(img_dirs):
+            img_features.append(extract_lseg_img_feature(img_dir, transform, evaluator))
+
+    n_points_cur = n_points
+    counter = torch.zeros((n_points_cur, 1), device=device)
+    sum_features = torch.zeros((n_points_cur, feat_dim), device=device)
+
+    ################ Feature Fusion ###################
+    vis_id = torch.zeros((n_points_cur, num_img), dtype=int, device=device)
+    for img_id, img_dir in enumerate(tqdm(img_dirs)):
+
+        # load pose
+        posepath = img_dir.replace('color', 'pose').replace('.jpg', '.txt')
+        pose = np.loadtxt(posepath)
+
+        # load depth and convert to meter
+        depth = imageio.v2.imread(img_dir.replace('color', 'depth').replace('jpg', 'png')) / depth_scale
+
+        # calculate the 3d-2d mapping based on the depth
+        mapping = np.ones([n_points, 4], dtype=int)
+        mapping[:, 1:4] = point2img_mapper.compute_mapping(pose, locs_in, depth)
+        if mapping[:, 3].sum() == 0: # no points corresponds to this image, skip
+            continue
+
+        mapping = torch.from_numpy(mapping).to(device)
+        mask = mapping[:, 3]
+        vis_id[:, img_id] = mask
+        
+        image = Image.open(img_dir).convert('RGB')
+        image = pil_to_tensor(image).to(dtype=torch.float32, device='cuda')
+        image.div_(255.0)
+        image = image.unsqueeze(dim=0)
+        
+        with torch.no_grad():
+            nearest_res = evaluator.get_nearest_supported_resolution(*image.shape[-2:])
+            image = F.interpolate(image, nearest_res, mode='bilinear', align_corners=False) 
+            summary, spatial_features = evaluator(image, feature_fmt='NCHW')
+            assert spatial_features.ndim == 4
+            feat_2d = spatial_features
+
+        feat_2d = torch.nn.functional.interpolate(
+            feat_2d, 
+            size=(240, 320), 
+            mode='bicubic', 
+            align_corners=False).squeeze(0) # 240, 320, 1024
+        feat_2d_3d = feat_2d[:, mapping[:, 1], mapping[:, 2]].permute(1, 0).to(device)
+
+        counter[mask!=0]+= 1
+        sum_features[mask!=0] += feat_2d_3d[mask!=0]
+
+    counter[counter==0] = 1e-5
+    feat_bank = sum_features/counter
+    point_ids = torch.unique(vis_id.nonzero(as_tuple=False)[:, 0])
+
+    
+    sameAs_save_fused_feature_scannet(feat_bank, point_ids, n_points, out_dir, scene_id, args)
+
+
+def main(args):   
+    seed = 1457
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+
+    #!### Dataset specific parameters #####
+    img_dim = (320, 240)
+    depth_scale = 1000.0
+    fx = 577.870605
+    fy = 577.870605
+    mx=319.5
+    my=239.5
+    #######################################
+    visibility_threshold = 0.25 # threshold for the visibility check
+
+    args.depth_scale = depth_scale
+    args.cut_num_pixel_boundary = 10 # do not use the features on the image boundary
+    args.keep_features_in_memory = False # keep image features in the memory, very expensive
+
+    split = args.split
+    data_dir = args.data_dir
+
+    data_root = join(data_dir, 'scannet_3d')
+    data_root_2d = join(data_dir,'scannet_2d')
+    args.data_root_2d = data_root_2d
+    out_dir = args.output_dir
+    args.feat_dim = 1024 # AMROIDIO feature dimension
+    os.makedirs(out_dir, exist_ok=True)
+    process_id_range = args.process_id_range
+
+    if split== 'train': # for training set, export a chunk of point cloud
+        args.n_split_points = 300000
+        args.num_rand_file_per_scene = 5
+    else: # for the validation set, export the entire point cloud instead of chunks
+        args.n_split_points = 2000000
+        args.num_rand_file_per_scene = 1
+
+
+    # model_version="radio_v2.5-h" # for RADIOv2.5-H model (ViT-H/16)
+    model_version="radio_v2.5-l" # for RADIOv2.5-L model (ViT-L/16)
+    #model_version="radio_v2.5-b" # for RADIOv2.5-B model (ViT-B/16)
+    #model_version="e-radio_v2" # for E-RADIO
+    model = torch.hub.load('NVlabs/RADIO', 'radio_model', version=model_version, progress=True, skip_validation=True)
+    model.cuda().eval()
+
+    args.evaluator = model
+
+    # calculate image pixel-3D points correspondances
+    intrinsic = make_intrinsic(fx=fx, fy=fy, mx=mx, my=my)
+    intrinsic = adjust_intrinsic(intrinsic, intrinsic_image_dim=[640, 480], image_dim=img_dim)
+
+
+    args.point2img_mapper = PointCloudToImageMapper(
+            image_dim=img_dim, intrinsics=intrinsic,
+            visibility_threshold=visibility_threshold,
+            cut_bound=args.cut_num_pixel_boundary)
+
+    data_paths = sorted(glob(join(data_root, split, '*.pth')))
+    total_num = len(data_paths)
+
+    id_range = None
+    if process_id_range is not None:
+        id_range = [int(process_id_range[0].split(',')[0]), int(process_id_range[0].split(',')[1])]
+
+    for i in trange(total_num):
+        if id_range is not None and \
+           (i<id_range[0] or i>id_range[1]):
+            print('skip ', i, data_paths[i])
+            continue
+
+        process_one_scene(data_paths[i], out_dir, args)
+
+if __name__ == "__main__":
+    args = get_args()
+    print("Arguments:")
+    print(args)
+
+    main(args)
